dc3497b5bdcc9edeb167715f9599ce536bf0c2ee
Emmanuelle Jim Arce <emmanuelle.arce@emmanuelle.arce-CKQ1H46KXL>
2022-10-05 10:35:02 +0800

    Added file index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const { square, fahrenhiet, rectangleArea, isPalindrome } = require('./lib');
+
+console.log(square(8));
+console.log(fahrenhiet(9));
+console.log(rectangleArea(5, 25));
+console.log(isPalindrome('LoL'));
+console.log(isPalindrome('Zack'));
--- lib.js
+++ lib.js
@@ -6,17 +6,24 @@ function fahrenhiet (celsius) {
     return (celsius * 1.8) + 32;
 }
 
-function recrangleArea (a, b) {
+function rectangleArea (a, b) {
     return (a * b);
 }
 
 function isPalindrome (str) {
-    str = str.toLower();
+    str = str.toLowerCase();
 
-    for (let i = 0; i < len/2; i++) {
-        if (str[i] !== str [len - 1 -i]) {
+    for (let i = 0; i < (str.length)/2; i++) {
+        if (str[i] !== str [(str.length) - 1 -i]) {
             return false; 
         } 
     } 
     return true;
+}
+
+module.exports = {
+    square,
+    fahrenhiet,
+    rectangleArea,
+    isPalindrome
 }
