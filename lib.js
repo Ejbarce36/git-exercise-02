e15f01d9c86c004f15e65302221a8b88d7e20039
Emmanuelle Jim Arce <emmanuelle.arce@emmanuelle.arce-CKQ1H46KXL>
2022-10-11 09:14:27 +0800

    added file .prettierrc and use prettier

--- lib.js
+++ lib.js
@@ -1,29 +1,29 @@
-function square (n) {
-    return square = n * n;
+function square(n) {
+  return (square = n * n);
 }
 
-function fahrenhiet (celsius) {
-    return (celsius * 1.8) + 32;
+function fahrenhiet(celsius) {
+  return celsius * 1.8 + 32;
 }
 
-function recrangleArea (a, b) {
-    return (a * b);
+function recrangleArea(a, b) {
+  return a * b;
 }
 
-function isPalindrome (str) {
-    str = str.toLowerCase();
+function isPalindrome(str) {
+  str = str.toLowerCase();
 
-    for (let i = 0; i < str.length/2; i++) {
-        if (str[i] !== str [str.length - 1 -i]) {
-            return false; 
-        } 
-    } 
-    return true;
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = {
-    square,
-    fahrenhiet,
-    recrangleArea,
-    isPalindrome
+  square,
+  fahrenhiet,
+  recrangleArea,
+  isPalindrome,
 };
